6aca8938f76e7e8a756eca39a66c3472312d27c7
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-06-21 02:55:56 +0000

    [add]15章

---
+++ app/models/post_image.rb
@@ -0,0 +1,16 @@
+class PostImage < ApplicationRecord
+  
+  has_one_attached :image
+  belongs_to :user
+  
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.jpg')
+      image_attach(io: File.open(file_path), filename:'default-image.jpg', content_type: "image/jpeg")
+    else
+      image
+    end
+  end
+  
+end
